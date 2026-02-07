e664bdfe68d678977a024806343c80bac2239b59
PaulsGitH <paullydee0@gmail.com>
2026-02-07 20:12:57 +0000

    Simple standalone lambda

---
+++ lambdas/simple.ts
@@ -0,0 +1,22 @@
+import { Handler } from "aws-lambda";
+
+export const handler: Handler = async () => {
+  try {
+    console.log("I was invoked");
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: { message: "This is a message returned from lambda Function" },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
